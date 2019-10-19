0d03e6c96ab4a4618e1bbe699977416a24237cd5
Romy Owens <rdoed160@gmail.com>
2019-10-19 15:11:43 -0400

    code axios bands in town for response and error log

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 // add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
+// add code to require axios package
+var axios = require("axios");
 
 // code required to import the `keys.js` file and store it in a variable
 var keys = require("./keys.js");
@@ -7,4 +9,28 @@ var keys = require("./keys.js");
 // access your keys information like so
 var spotify = new Spotify(keys.spotify);
 
+// create variable to take in commands
+var command = (process.argv[2]);
+var artist = (process.argv[3]);
 
+
+
+// setup axios for Bnds in Town API
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
+    console.log("Here's what we got " + response.data);
+})
+.catch(function (error) {
+    if (error.response) {
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+    } else if (error.request) {
+        console.log(error.request);
+    } else {
+        console.log("Error", error.message);
+    }
+    console.log(error.config);
+});
