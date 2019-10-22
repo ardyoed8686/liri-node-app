d38580b78e84df9bdff030e0218f2284865520a9
Romy Owens <rdoed160@gmail.com>
2019-10-21 20:25:00 -0400

    setup log of OMDB results

--- liri.js
+++ liri.js
@@ -5,26 +5,26 @@ require("dotenv").config();
 var axios = require("axios");
 
 // code required to import the `keys.js` file and store it in a variable
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
 // get node package for read and write of text files
 var fs = require("fs");
 
 // access your keys information like so
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 // create variable to take in commands
 var command = (process.argv[2]);
 var userInput = (process.argv[3]);
 
-switch (action) {
+switch (command) {
   case "concert-this":
     concert();
     break;
   
-  case "spotify-this-song":
-    spotify();
-    break;
+  // case "spotify-this-song":
+  //   spotify();
+  //   break;
   
   case "movie-this":
     movie();
@@ -51,18 +51,18 @@ function concert() {
 
 
 // setup Spotify call
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-    spotify.search({type: 'track', query: 'The Sign'},)
-  }
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//     spotify.search({type: 'track', query: 'The Sign'},)
+//   }
  
-console.log(data); 
-console.log(data.name)
-// console log song name, preview link, album
+// console.log(data); 
+// console.log(data.name)
+// // console log song name, preview link, album
 
 
-});
+// });
 
 
 
@@ -75,19 +75,22 @@ var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey
 // Request with axios to the OMDB API with the movie specified
 
 axios.get(queryUrl).then(function(response) {
-console.log(queryUrl);
-console.log("Your movie data" + response.data);
-console.log(response.data.Year);
-// console log 
-// * Title of the movie.
-// * Year the movie came out.
-// * IMDB Rating of the movie.
-// * Rotten Tomatoes Rating of the movie.
-// * Country where the movie was produced.
-// * Language of the movie.
-// * Plot of the movie.
-// * Actors in the movie.
+
+console.log("------------------");
+console.log("Title: " + response.data.Title);
+console.log("Year: " + response.data.Year);
+console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+console.log("Country: " + response.data.Country);
+console.log("Language: " + response.data.Language);
+console.log("Plot: " + response.data.Plot);
+console.log("Actors: " + response.data.Actors);
+console.log("------------------");
 });
+if (userInput === "") {
+  userInput = Mr. Nobody;
+  
+}
   //  * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 };
 
